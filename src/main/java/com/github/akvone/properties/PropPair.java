e09eee53c41916b13844ba22b61af832ebe08cb3
Kirill Vialkov <akvone@mail.ru>
2020-04-24 22:10:39 +0300

    Add lombok dependency

--- src/main/java/com/github/akvone/properties/PropPair.java
+++ src/main/java/com/github/akvone/properties/PropPair.java
@@ -1,23 +1,14 @@
 package com.github.akvone.properties;
 
+import lombok.Data;
+
+@Data
 public class PropPair {
+
   private final String yamlFile;
   private final boolean required;
 
-  public PropPair(String yamlFile, boolean required) {
-    this.yamlFile = yamlFile;
-    this.required = required;
-  }
-
-  public static PropPair of(String yamlFile, boolean required){
+  public static PropPair of(String yamlFile, boolean required) {
     return new PropPair(yamlFile, required);
-  }
-
-  public String getYamlFile() {
-    return yamlFile;
-  }
-
-  public boolean isRequired() {
-    return required;
   }
 }
